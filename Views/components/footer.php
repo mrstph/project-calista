1458542bf56e2327448aa1c0a023e1256af1567e
mrstph <118338282+mrstph@users.noreply.github.com>
2023-01-10 16:43:01 +0100

    add nav file, modification css file

---
+++ Views/components/footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="container">
+        <div class="row col-sm-9 col-lg-5 mx-auto text-center mb-4">
+            <a href="#" class="col-sm-12 col-lg-6 mb-3">Mentions légales</a>
+            <a href="#" class="col-sm-12 col-lg-6">Politique de confidentialité</a>
+        </div>
+        <div class="text-center">
+        <p>&#169; 2023 Calista - Tous droits réservés.</p>
+    </div>
+</footer>
---
+++ Views/components/nav.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg navbar-dark">
+    <div class="container gap-3">
+        <a class="navbar-brand" href="#"><img id="logo-nav" src="assets/pictures/calista-logo-white.svg" alt="Calista, application Trello like"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a id="dashboard-nav" class="nav-link active" href="#">Mon espace de travail
+                    </a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a id="boards-nav" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Mes tableaux</a>
+                    <div class="dropdown-menu">
+                    <!-- code that retrieves the tables  -->
+                        <a class="dropdown-item" href="#">Tableau 1</a>
+                        <a class="dropdown-item" href="#">Tableau 2</a>
+                    </div>
+                </li>
+            </ul>
+            <ul class="navbar-nav gap-2">
+                <li class="nav-item">
+                    <a class="nav-link" id="help-icon-nav" href="">Guide de Calista
+                    </a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><img id="profile-icon-nav" src="assets/pictures/profile-picture.svg" alt="Menu de profil"><span id="nav-profile-name">Chris Balla</span></a>
+                    <div class="dropdown-menu dropdown-menu-end">
+                        <a class="dropdown-item" href="#">Profil</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="logout.php">Se déconnecter</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
